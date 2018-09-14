ce7d3f231c7b90e6b431a7b4fe039bb9022a284a
sssunho <sssunho.lee@gmail.com>
2018-09-15 00:09:24 +0900

    HYCON HACKS

--- README.md
+++ README.md
@@ -1,2 +1,92 @@
 # M-MChain-server
 HYCON HACKS
+
+## API
+
+### /post
+장소에 사진 등록하고 토큰 얻기. 5 Maro Token이 지급됩니다.
+
+#### request
+POST
+
+**Body**
+
+- user: hycon wallet address
+- longitude: 경도
+- latitude: 위도
+- tags: 태그, list
+- img: 사진, file
+- memo
+
+#### response
+
+```json
+{
+    "latitude": "0.1234566",
+    "longitude": "0.6543211",
+    "memo": "여기가 KBS 아레나홀의 정문입니다.",
+    "place_id": 7,  # 등록된 장소 번호
+    "tags": [ "입구", "정문" ],
+    "transaction_hash": "생성된 트랜잭션의 해시값"
+}
+```
+
+
+
+### /get
+입력된 위치의 반경 범위 내에 등록된 장소 목록 조회
+
+#### request
+GET
+
+**Query String**
+
+- longitude: 경도
+- latitude: 위도
+- range: 반경 범위, float
+
+```
+decimal  degrees    distance
+-------------------------------
+0        1.0        111 km
+1        0.1        11.1 km
+2        0.01       1.11 km
+3        0.001      111 m
+4        0.0001     11.1 m
+5        0.00001    1.11 m
+6        0.000001   0.111 m
+7        0.0000001  1.11 cm
+8        0.00000001 1.11 mm
+```
+
+#### response
+
+```json
+{
+    "places": [
+        {
+            "create_time": "Fri, 14 Sep 2018 12:48:40 GMT",
+            "id": 6,  # 장소 번호
+            "img_name": "moon.jpg",  # 이미지 이름
+            "latitude": 0.123457,
+            "longitude": 0.654321,
+            "memo": "memo",
+            "tags": "one,two",
+            "user_address": "hycon wallet address"
+        }, ...
+}
+```
+
+
+
+### /img/$place_id
+장소에 저장된 이미지를 조회한다
+
+#### request
+GET
+
+- $place_id: 장소 번호
+
+#### response
+
+img
---
+++ app.py
@@ -0,0 +1,129 @@
+from flask import Flask, request, make_response, jsonify, send_file
+from gevent.wsgi import WSGIServer
+import requests
+import traceback
+import pymysql
+import json
+from io import BytesIO
+
+app = Flask(__name__)
+
+with open('config.json', 'r') as f:
+    conf = json.load(f)
+DATABASE = conf['mysql']
+HYCON = conf['hycon']
+
+
+def connect_db():
+    return pymysql.connect(host=DATABASE['host'], 
+            user=DATABASE['user'], 
+            password=DATABASE['password'], 
+            db=DATABASE['database'],
+            charset='utf8', 
+            cursorclass=pymysql.cursors.DictCursor)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/post', methods=['POST'])
+def post():
+    # 위치, 사진, 태그, 사용자
+    user_address = request.values.get('user', None)  # 토큰받을 사용자의 address
+    longitude = request.values.get('longitude', None)  # 경도
+    latitude = request.values.get('latitude', None)  # 위도
+    memo = request.values.get('memo', None)
+    
+    taglist = request.values.getlist('tags', None)
+    tags = ','.join(taglist)
+
+    img = request.files.get('img', None)
+    img_name = img.filename
+    img_mimetype = img.content_type
+
+    # 입력받은 데이터 저장하기
+    try:
+        conn = connect_db()
+        cur = conn.cursor()
+        query = """INSERT INTO places (longitude, latitude, img, img_name, img_mimetype, user_address, tags, memo) 
+                VALUES(%s, %s, %s, %s, %s, %s, %s, %s);"""
+        ret = cur.execute(query, (longitude, latitude, img.read(), img_name, img_mimetype, user_address, tags, memo, ))
+        place_id = cur.lastrowid
+
+        # 5 MT 발행하기
+        api_hycon_transaction = "http://fabius.ciceron.xyz:2442/api/v1/signedTx"
+        payload = {
+            "privateKey": HYCON['privateKey'],
+            "from": HYCON['minerAddress'],
+            "to": user_address,
+            "amount": "0.000000005",
+            "fee": "0.000000005"
+        }
+        r = requests.post(api_hycon_transaction, json=payload)
+
+        conn.commit()
+    except:
+        traceback.print_exc()
+        conn.rollback()
+        error = {
+            "code": 2222,
+            "type": "ServerError",
+            "message": "Server is temporary unavailable."
+        }
+        return make_response(jsonify(error=error), 500)
+    
+    if r.status_code != 200:
+        error = {
+            "code": 1111,
+            "type": "HyconError",
+            "message": "Hycon server is temporary unavailable."
+        }
+        return make_response(jsonify(error=error), 500)
+    else:
+        rr = r.json()
+        data = {
+            "place_id": place_id,
+            "transaction_hash": rr['txHash'],
+            "longitude": longitude,
+            "latitude": latitude,
+            "memo": memo,
+            "tags": taglist
+        }
+        return make_response(jsonify(**data), 200)
+
+
+@app.route('/get', methods=['GET'])
+def get():
+    # 현재 위치, 반경 범위
+    longitude = request.values.get('longitude', None)  # 경도
+    longitude = float(longitude)
+    latitude = request.values.get('latitude', None)  # 위도
+    latitude = float(latitude)
+    range = request.values.get('range', None)
+    range = float(range)
+    
+    # 현재 위치 기준 반경 범위안에 있는 곳 DB에서 꺼내기
+    conn = connect_db()
+    cur = conn.cursor()
+    query = """SELECT id, longitude, latitude, img_name, user_address, tags, memo, create_time FROM mmchain.places
+            WHERE (longitude BETWEEN %s AND %s) AND (latitude BETWEEN %s AND %s);"""
+    cur.execute(query, (longitude-range, longitude+range, latitude-range, latitude+range))
+    ret = cur.fetchall()
+    return make_response(jsonify(places=ret), 200)
+
+
+@app.route('/img/<int:place_id>', methods=['GET'])
+def img(place_id):
+    conn = connect_db()
+    cur = conn.cursor()
+    query = "SELECT img, img_name, img_mimetype FROM mmchain.places WHERE id = %s;"
+    cur.execute(query, (place_id, ))
+    ret = cur.fetchone()
+
+    return send_file(BytesIO(ret['img']), attachment_filename=ret['img_name'])
+
+
+if __name__ == '__main__':
+    # app.run()
+    http = WSGIServer(('0.0.0.0', 5000), app)
+    http.serve_forever()
